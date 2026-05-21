e1cfab559c739d3c19e98b16979769a2384fd017
Pedro Canuto <data.canuto@gmail.com>
2026-05-21 19:39:54 -0300

    start project
    start project by spring initializr
    dependencies:
    -JPA
    -WEB
    -Validation
    -H2
    
    Entities
    -Paciente
    -Prontuario
    
    Relation oneToOne
    Validation (NotBlank fields)
    Start dba in /h2-console/

---
+++ gerenciamento-prontuarios/src/main/java/com/clinica/gerenciamento_prontuarios/GerenciamentoProntuariosApplication.java
@@ -0,0 +1,13 @@
+package com.clinica.gerenciamento_prontuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoProntuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoProntuariosApplication.class, args);
+	}
+
+}
---
+++ gerenciamento-prontuarios/src/test/java/com/clinica/gerenciamento_prontuarios/GerenciamentoProntuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinica.gerenciamento_prontuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoProntuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
