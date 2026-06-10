98a92e15fa3dfd6498ff9bd470c7ccff540f5130
Pedro Canuto <data.canuto@gmail.com>
2026-06-09 21:09:13 -0300

    add files via upload

---
+++ apicimatec/apicimatec/src/test/java/com/senai/apicimatec/ApicimatecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.apicimatec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApicimatecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
